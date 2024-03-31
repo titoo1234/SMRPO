e326dee8e323b4ddbb4734f8764a78248832eec9
titoo1234 <tito.arnsek@gmail.com>
2024-03-31 11:22:00 +0200

    Dodajanje default sprinta

--- scrum/migrations/0025_default_data.py
+++ scrum/migrations/0025_default_data.py
@@ -1,11 +1,13 @@
 import datetime
 from django.db import migrations
-from ..models import User,Project,AssignedRole,ProjectMember
+from ..models import User,Project,AssignedRole,ProjectMember,Sprint
 
 def create_default_data(apps, schema_editor):
     admin = User.objects.get(username='ad')
     uporabnik = User.objects.get(username='uporabnik')
     uporabnik1 = User.objects.get(username='Veseli Ribnčan')
+    fixed_date = datetime.datetime(2024, 3, 25)
+
     project = Project.objects.create(name='Default Project', creation_date=datetime.datetime.now(),  description='Default project description', creator=admin)
     ProjectMember.objects.create(user=admin, project=project)
     ProjectMember.objects.create(user=uporabnik1, project=project)
@@ -15,7 +17,12 @@ def create_default_data(apps, schema_editor):
     AssignedRole.objects.create(user=uporabnik1, project=project, role='development_team_member')
     AssignedRole.objects.create(user=uporabnik, project=project, role='development_team_member')
 
-    
+    # SPRINT
+    # =============================================
+    sprint_start_date = datetime.datetime(2024, 3, 26)
+    sprint_end_date = datetime.datetime(2024, 4, 10)
+    default_sprint = Sprint.objects.create(project=project, start_date=sprint_start_date, end_date=sprint_end_date,velocity = 15)
+
     
 class Migration(migrations.Migration):
 
